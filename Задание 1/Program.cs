833236fe1338a7f38997b32507222123b372efb9
Сидоров Артем <sidoroffartemii@gmail.com>
2024-08-15 22:06:13 +0300

    Добавьте файлы проекта.

---
+++ Задание 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус окружности");
+            double R =double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координаты точки А");
+            double X=double.Parse(Console.ReadLine());
+            double Y=double.Parse(Console.ReadLine());
+
+            double Length= circle.GetLength(R);
+            double Squre = circle.GetSqure(R);
+            Console.WriteLine ($"Длина окружности ={Length}, Площадь круга ={Squre}");
+            circle.GetKoordinat(R,X,Y);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Задание 1/circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_1
+{
+    public static class circle
+    {
+        static public double GetLength(double r)
+        {
+            return 2*Math.PI*r;
+
+        }
+
+        static public double GetSqure(double r)
+        {
+            return Math.PI * r*r;
+
+        }
+        static public void GetKoordinat(double r, double x, double y)
+        {
+
+            if ((x)*(x) + (y) * (y) <= r * r)
+                Console.WriteLine("Точка принадлежит окружности");
+           else Console.WriteLine ("Точка не принадлежит окружности");
+            
+        }
+    }
+}
